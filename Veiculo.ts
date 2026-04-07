1472c377b5f79314c459ed39c9921a8f18ada980
marianatica <marianatica.va007@academico.ifsul.edu.br>
2026-04-06 23:35:21 -0300

    feat: adiciona função acelerar

--- Veiculo.ts
+++ Veiculo.ts
@@ -5,4 +5,17 @@ export class Veiculo{
     numeroMarchas: number = 5;
     marchaAtual: number = 0;
     velocidade: number = 0;
+
+    acelerar(incremento: number): void {
+        if (this.marchaAtual === 0) {
+            console.log("Coloque o veículo em uma marcha antes de acelerar");
+            return;
+        }
+        if (incremento <= 0) {
+            console.log("Valor inválido para acelerar");
+            return;
+        }
+        this.velocidade += incremento;
+        console.log(`Acelerando... Velocidade: ${this.velocidade} km/h`);
+    }
 }
