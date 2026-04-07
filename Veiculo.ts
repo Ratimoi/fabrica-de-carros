8f5a6886f37f616db38541a8a74770fb2188485c
marianatica <marianatica.va007@academico.ifsul.edu.br>
2026-04-07 00:07:33 -0300

    feat: arrumando a função acelelar

--- Veiculo.ts
+++ Veiculo.ts
@@ -6,16 +6,4 @@ export class Veiculo{
     marchaAtual: number = 0;
     velocidade: number = 0;
 
-    acelerar(incremento: number): void {
-        if (this.marchaAtual === 0) {
-            console.log("Coloque o veículo em uma marcha antes de acelerar");
-            return;
-        }
-        if (incremento <= 0) {
-            console.log("Valor inválido para acelerar");
-            return;
-        }
-        this.velocidade += incremento;
-        console.log(`Acelerando... Velocidade: ${this.velocidade} km/h`);
-    }
 }
